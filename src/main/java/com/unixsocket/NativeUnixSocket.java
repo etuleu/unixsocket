512eeb9c08d3faa69a37a64a549b81b04cf58549
Erdal Tuleu <etuleu@gmail.com>
2020-02-28 12:59:49 -0800

    update docs for native java class

--- src/main/java/com/unixsocket/NativeUnixSocket.java
+++ src/main/java/com/unixsocket/NativeUnixSocket.java
@@ -16,68 +16,58 @@ public class NativeUnixSocket {
     /*
      * Creates a unix socket.
      * Returns the file descriptor of new the socket.
-     *
      */
     public static native int socket();
 
     /*
-     * TODO
-     * Maybe a more abstract name would be better
-     *
+     * Create the epoll object, which can be used to read/write from sockets
+     * in an async manner.
      */
     public static native int epollCreate();
 
     /*
-     * TODO
-     *
-     *
+     * Register a new socket with the epoll object.
      */
     public static native int epollAdd(int epollfd, int fd);
 
-    // TODO: epollRemove ? epollModify?
-
     /*
-     * TODO
+     * Wait for events from the registered sockets.
      *
-     * We might want a timeout parameter passed in
+     * TODO: We might want a timeout parameter passed in
      */
     public static native int[] epollWait(int epollfd);
 
     /*
      * Unix socket connect
-     *
      */
     public static native int connect(int fd, String host);
 
     /*
      * Read from socket at file descriptor fd into data.
-     * Returns number of bytes read or negative error.
-     *
+     * Returns number of bytes read or negative errno.
      */
     public static native int read(int fd, byte[] data);
 
     /*
      * Unix socket write
-     *
+     * Returns number of bytes writtern or negative errno.
      */
     public static native int write(int fd, byte[] data);
 
     /*
      * Mark the socket at file descriptor fd as blocking or non-blocking.
-     *
      */
     public static native int setBlocking(int fd, boolean block);
 
     /*
      * Unix socket close
-     *
      */
     public static native int close(int fd);
 
 
     /*
      * Unix socket server
-     *
+     * (only used in testing)
      */
     public static native int startlistening(String host);
 }
